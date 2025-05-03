2613759a5e6aa80d917b060501d3707c39e7a7f3
MinorProject-img <minorproject821@gmail.com>
2025-05-03 18:21:29 +0530

    add

--- function.py
+++ function.py
@@ -35,9 +35,9 @@ def extract_keypoints(results):
 # Path for exported data, numpy arrays
 DATA_PATH = os.path.join('MP_Data') 
 
-actions = np.array(['A','B','C','D'])
+actions = np.array(['A','B','C'])
 
-no_sequences = 30
+no_sequences =100
 
-sequence_length = 30
+sequence_length = 100
 
